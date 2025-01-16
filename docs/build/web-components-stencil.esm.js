b097a25e9f01f51f3fee611a0c9d7552bd80e7b2
Matheus de Deus Rocha <matheus.rocha@sis-it.com>
2025-01-16 17:41:00 -0300

    Update files

--- docs/build/web-components-stencil.esm.js
+++
@@ -1,2 +0,0 @@
-import{p as r,b as e}from"./p-c4b7b123.js";export{s as setNonce}from"./p-c4b7b123.js";import{g as t}from"./p-e1255160.js";var o=()=>{const e=import.meta.url;const t={};if(e!==""){t.resourcesUrl=new URL(".",e).href}return r(t)};o().then((async r=>{await t();return e([["p-3fcfe3b6",[[1,"md-side-drawer",{drawerTitle:[513,"drawer-title"],open:[1540],showContactInfo:[32],openDrawer:[64]}]]]],r)}));
-//# sourceMappingURL=web-components-stencil.esm.js.map
