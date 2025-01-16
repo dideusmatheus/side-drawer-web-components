b6f9829948bd2fd5e5c36caec5ce4c465b4bebd2
Matheus de Deus Rocha <matheus.rocha@sis-it.com>
2025-01-16 11:09:25 -0300

    Creating methods for close drawer and adding styles

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -8,22 +8,23 @@ import { Component, Host, Prop, h } from '@stencil/core';
 export class SideDrawer {
 
   @Prop({reflect: true}) title: string; // utilize reflect quando quiser alterar a props e alterar o valor no DOM
-  @Prop() open: boolean;
+  @Prop({reflect: true, mutable: true}) open: boolean;
+
+  onCloseDrawer(){
+    this.open = false;
+  }
 
   render() {
-    let content = null;
-    if(this.open){
-      content = (
-        <aside>
-          <header>
-            <h1> { this.title }</h1>
-          </header>
-          <main>
-            <slot></slot>
-          </main>
-        </aside>
-      );
-    }
-    return content;
+    return (
+      <aside>
+        <header>
+          <h1> { this.title }</h1>
+          <button onClick={ this.onCloseDrawer.bind(this)}> X </button>
+        </header>
+        <main>
+          <slot></slot>
+        </main>
+      </aside>
+    );
   }
 }
